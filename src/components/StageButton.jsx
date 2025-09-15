a41525ec3835de9f18ef3a687e9d24d86c007348
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2025-09-15 14:13:27 +0200

    Create stage button component

---
+++ src/components/StageButton.jsx
@@ -0,0 +1,17 @@
+import './StageButton.css';
+
+function StageButton() {
+    return (
+        <button
+            className="background"
+            style={{
+                color: 'white',
+                marginTop: '1.5rem',
+                borderRadius: '3px',
+                fontWeight: '600',
+            }}
+        >Jetzt sichern</button>
+    );
+}
+
+export default StageButton
