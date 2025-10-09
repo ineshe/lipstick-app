5673f336698de3721baf0d6a39c404dea97c9c6a
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2025-10-09 14:33:59 +0200

    Change lipstick title on stage

--- src/components/StageText.jsx
+++ src/components/StageText.jsx
@@ -3,19 +3,14 @@ import './StageText.css';
 
 function Stage() {
     return (
-        <div
-            className='stage-wrapper' 
-        >
+        <div className='stage-wrapper'>
             <div className='stage-content'>
-                <h1
-                    className='stage-headline'
-                >
-                    Velvet Lips Color Crush
+                <h1 className='stage-headline'>
+                    <span>Lumines Color Crush</span>
                 </h1>
-                <p
-                    className='stage-subline'
-                >
-                    Mauris sit amet risus faucibus, pharetra arcu</p>
+                <p className='stage-subline'>
+                    Mauris sit amet risus faucibus, pharetra arcu
+                </p>
                 <StageButton />
             </div>
         </div>
