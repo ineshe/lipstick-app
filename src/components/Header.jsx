0be847a2b103ff93c1fac800decb8efd736e5a6f
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2025-11-12 17:36:26 +0100

    Use motion/react for animation and add a top animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useScroll, useMotionValueEvent } from 'motion/react';
+import { motion, useScroll, useMotionValueEvent } from 'motion/react';
 import { useState } from 'react';
 import './Header.css';
 
@@ -6,14 +6,44 @@ function Header() {
 
     const { scrollY } = useScroll()
     const [hidden, setHidden] = useState(false)
+    const [top, setTop] = useState(true)
 
     useMotionValueEvent(scrollY, "change", (current) => {
         const diff = current - scrollY.getPrevious()
         setHidden(diff > 0 ? true : false)
+        setTop(current < 1 ? true : false)
     })
 
+    const variants = {
+        hidden: { 
+            y: -100, 
+            opacity: 0,
+            transition: { duration: 0.18, ease: "easeInOut" }
+        },
+        top: {
+            y: 0, opacity: 1,
+            backgroundColor: 'rgba(0,0,0,0)',
+            // boxShadow: 'none',
+            backdropFilter: 'none',
+            WebkitBackdropFilter: 'none'
+        },
+        scrolled: {
+            y: 0, opacity: 1,
+            backgroundColor: 'rgba(0,0,0,0.2)',
+            // boxShadow: '0 4px 16px rgba(0,0,0,0.15)',
+            backdropFilter: 'blur(5px)',
+            WebkitBackdropFilter: 'blur(5px)',
+            transition: { duration: 0.36, ease: "easeInOut" }
+        }
+    }
+
+    const activeVariant = hidden ? "hidden" : (top ? "top" : "scrolled")
+
     return (
-        <div className={`top-bar-wrapper ${hidden ? "hidden" : ""}`}>
+        <motion.div className={'header-wrapper'}
+            variants={variants}
+            animate={activeVariant}
+        >
             <div className='top-bar'>
                 <nav className='menu'>
                     <ul className='menu-list'>
@@ -24,7 +54,7 @@ function Header() {
                     </ul>
                 </nav>
             </div>
-        </div>
+        </motion.div>
     );
 }
 
