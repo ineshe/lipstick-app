3281cae9685def80bdc05a7d49c8dbc9d6bb0d73
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2025-12-03 15:55:37 +0100

    Position usps with grid

--- src/components/UspList.jsx
+++ src/components/UspList.jsx
@@ -4,36 +4,28 @@ import './UspList.css';
 function UspList() {
     const usps = [{
         id: 0,
-        top: '30%',
         headline: 'Pflegend',
         content: 'Intensive Farbe trifft auf pflegende Inhaltsstoffe – für samtweiche Lippen mit jedem Auftrag.'
     }, {
         id: 1,
-        top: '50%',
         headline: 'Langanhaltend',
         content: 'Strahlende Lippenfarbe, die den ganzen Tag hält – ohne Nachschminken, ohne Kompromisse.'
     }, {
         id: 2,
-        top: '70%',
         headline: 'Glanz-Effekt',
         content: 'Spieglein, Glanz – unwiderstehlich strahlende Lippen mit nur einem Auftrag.'
     }];
 
     const uspList = usps.map((usp) => {
-        const side = usp.id % 2 === 0 ? 'left' : 'right';
-
         return (
             <motion.div 
-                className={`usp-item ${side}`}
-                style={{
-                    top: usp.top,
-                }}
+                className='usp-item'
                 key={usp.id}
                 initial= {{ opacity: 0 }}
                 whileInView={{ 
                     opacity: 1,
                     transition: { 
-                        duration: 0.1,
+                        duration: 0.8,
                         default: { ease: 'easeOut' }
                     }
                 }}
@@ -46,7 +38,7 @@ function UspList() {
     });
 
     return (
-        <div className="usp-list-wrapper viewport-content">
+        <div className="usp-list-wrapper">
             {uspList}
         </div>
     );
