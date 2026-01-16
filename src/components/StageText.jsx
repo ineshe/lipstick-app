f631db09713091faf561d6089791e1472a40e744
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2026-01-16 15:34:37 +0100

    Add fonts and adjust styles

--- src/components/StageText.jsx
+++ src/components/StageText.jsx
@@ -7,9 +7,12 @@ function Stage() {
             <div className='stage-wrapper viewport-content'>
                 <div className='stage-content'>
                     <div className='stage-text'>
-                        <h1 className='stage-headline'>
-                            Lumines Lips&reg;
-                        </h1>
+                        <div className='page-title'>
+                            <h1 className='stage-headline'>
+                                Lumines Lips
+                            </h1>
+                            <span id="r">&reg;</span>
+                        </div>
                         <p className='stage-subline'>
                             Mauris sit amet risus faucibus, pharetra arcu sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat
                         </p>
