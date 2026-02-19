313ae5554aa07b152bdceda1ec38a055c71f725b
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2026-02-19 16:09:33 +0100

    feat: add chevron-right icon and integrate into StageButton component

--- src/components/StageButton/StageButton.jsx
+++ src/components/StageButton/StageButton.jsx
@@ -1,10 +1,12 @@
 import './StageButton.css';
+import ChevronRight from '../../assets/icons/chevron-right.svg';
 
 function StageButton() {
     return (
-        <button
-            className="stage-button glow-on-hover"
-        >Jetzt sichern</button>
+        <button className="stage-button glow-on-hover">
+            Jetzt sichern
+            <ChevronRight size={50} />
+        </button>
     );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import createSvgSpritePlugin from 'vite-plugin-svg-sprite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createSvgSpritePlugin({
+      exportType: 'react',
+      include: '**/icons/*.svg',
+    }),
+  ],
 })
