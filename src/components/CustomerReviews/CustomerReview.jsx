e927783bfae06b3cf23a2c9a4231901d1fd86739
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2026-03-12 13:29:39 +0100

    fix(CustomerReview): remove commented author city and adjust review content formatting

--- src/components/CustomerReviews/CustomerReview.jsx
+++ src/components/CustomerReviews/CustomerReview.jsx
@@ -8,12 +8,12 @@ function CustomerReview({ review }) {
                 <div className="review-icon"></div>
                 <div>
                     <p className="author-name">{review.authorName}, {review.authorAge}</p>
-                    <p className="author-city">aus {review.authorCity}</p>
+                    {/* <p className="author-city">aus {review.authorCity}</p> */}
                 </div>
             </div>
             <hr className='line'/>
             <p className="review-title">{review.title}</p>
-            <p className="review-content">"{review.content}"</p>
+            <p className="review-content">{review.content}</p>
         </>
     );
 }
