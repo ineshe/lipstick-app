d07c09e04f7080c3159680e70c269ceaa93d1807
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2026-03-16 14:25:54 +0100

    fix(StageButton, StageText): adjust button positioning and subline margin for improved layout

--- src/components/StageText/StageText.jsx
+++ src/components/StageText/StageText.jsx
@@ -1,19 +1,21 @@
 import { useRef, useEffect } from 'react';
 import { motion, useInView, useSpring } from 'motion/react';
+import { useIsMobile } from '../../hooks/use-is-mobile';
 import StageButton from '../StageButton';
 import './StageText.css';
 
 function Stage() {
     const ref = useRef(null);
+    const { isMobile } = useIsMobile();
     const isInView = useInView(ref, { amount: 0.88 });
     
     const x = useSpring(0, { stiffness: 180, damping: 35, visualDuration: 0.2 });
     const buttonSkewX = useSpring(0, { stiffness: 180, damping: 35, visualDuration: 0.2 });
     
     useEffect(() => {
-        x.set(isInView ? 0 : window.innerWidth);
-        buttonSkewX.set(isInView ? 0 : -15);
-    }, [isInView, x, buttonSkewX]);
+        x.set(isInView || isMobile ? 0 : window.innerWidth);
+        buttonSkewX.set(isInView || isMobile ? 0 : -15);
+    }, [isInView, isMobile, x, buttonSkewX]);
 
     return (
         <div className='stage-wrapper viewport-content' ref={ref}>
@@ -22,12 +24,9 @@ function Stage() {
                 style={{ x }}
             >
                 <div className='stage-text'>
-                    <div className='page-title'>
-                        <h1 className='stage-headline'>
-                            Lumines&nbsp;Lips<span id="copyright">&reg;</span>
-                        </h1>
-                        
-                    </div>
+                    <h1 className='stage-headline'>
+                        Lumines&nbsp;Lips<span id="copyright">&reg;</span>
+                    </h1>
                     <p className='stage-subline'>
                         Mauris sit amet risus faucibus, pharetra arcu sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat
                     </p>
